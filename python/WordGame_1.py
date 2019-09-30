9d85da2f900b43805b7e8cf4aeb31f6bfa2b2c17
mht <mouhaotian@weconex.com>
2019-09-30 21:09:05 +0800

    初始提交

---
+++ python/WordGame_1.py
@@ -0,0 +1,8 @@
+print('-------------------------hello wold--------------------------')
+temp = input("猜一个数字")
+guess = int(temp)
+if guess == 8:
+    print("猜对了")
+else:
+    print("猜错了")
+print("游戏结束")
